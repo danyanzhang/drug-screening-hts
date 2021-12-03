0443b0dbfb128e6ee92f3e635a14077b3d32cc88
Dan Zhang <43554393+danyanzhang@users.noreply.github.com>
2021-12-03 10:07:44 -0500

    Changed to pull data from internal source instead of external

--- importData.m
+++ importData.m
@@ -1,6 +1,13 @@
+function data = importData()
+    data = readtable('data-invert.csv');
+end
+
+
+%{
 % go up to previous directory
 function data = importData()
 addpath(['..', filesep, 'DataDerived'])
 data = readtable('james_CL_invert.csv');
 rmpath(['..', filesep, 'DataDerived']);
 end
+%}
